685192c54b63671dd83a7f2db9374fb9786a16a3
RenatoDiasc <115375244+RenatoDiasc@users.noreply.github.com>
2025-09-05 21:46:25 -0300

    feature: adicionado ao DTO os atributos getters,setters e constructor que serão utilizados pelo mapper

--- src/main/java/dev/rdiaz/MagicFridgeAi/dto/FoodDTO.java
+++ src/main/java/dev/rdiaz/MagicFridgeAi/dto/FoodDTO.java
@@ -1,4 +1,58 @@
 package dev.rdiaz.MagicFridgeAi.dto;
 
+import dev.rdiaz.MagicFridgeAi.categorias.FoodCategoria;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class FoodDTO {
+
+    private Long id;
+    private String nome;
+    private FoodCategoria categoria;
+    private Integer quantidade;
+    private LocalDate validade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public FoodCategoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(FoodCategoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
 }
