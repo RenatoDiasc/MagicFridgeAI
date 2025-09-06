a43df72878344067114d683e78c0cc845dc57dac
RenatoDiasc <115375244+RenatoDiasc@users.noreply.github.com>
2025-09-05 22:31:28 -0300

    feature: feito o mapeamento do foodDTO para model e vice versa

--- src/main/java/dev/rdiaz/MagicFridgeAi/mapper/FoodMapper.java
+++ src/main/java/dev/rdiaz/MagicFridgeAi/mapper/FoodMapper.java
@@ -1,4 +1,36 @@
 package dev.rdiaz.MagicFridgeAi.mapper;
 
+import dev.rdiaz.MagicFridgeAi.dto.FoodDTO;
+import dev.rdiaz.MagicFridgeAi.model.FoodItem;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FoodMapper {
+
+    public FoodItem map(FoodDTO foodDTO){
+
+        FoodItem foodItem = new FoodItem();
+
+        foodItem.setId(foodDTO.getId());
+        foodItem.setNome(foodDTO.getNome());
+        foodItem.setCategoria(foodDTO.getCategoria());
+        foodItem.setQuantidade(foodDTO.getQuantidade());
+        foodItem.setValidade(foodDTO.getValidade());
+
+        return foodItem;
+    }
+
+    public FoodDTO map(FoodItem foodItem){
+
+        FoodDTO foodDTO = new FoodDTO();
+
+        foodDTO.setId(foodItem.getId());
+        foodDTO.setNome(foodItem.getNome());
+        foodDTO.setCategoria(foodItem.getCategoria());
+        foodDTO.setQuantidade(foodItem.getQuantidade());
+        foodDTO.setValidade(foodItem.getValidade());
+
+        return foodDTO;
+    }
+
 }
